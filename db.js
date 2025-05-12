0bc4bd4d2768a87d2374d9a2ade2f2975e6d097f
anirudhVASUDEV23 <anirudh.vasudev.kesana@gmail.com>
2025-05-12 22:16:06 +0530

    Corrected Primary Key Error

--- db.js
+++ db.js
@@ -18,7 +18,7 @@ connection.connect((err) => {
   // Create table if it doesn't exist (without 'distance' column)
   const createTableQuery = `
     CREATE TABLE IF NOT EXISTS schools (
-      id INT PRIMARY KEY,
+      id INT AUTO_INCREMENT PRIMARY KEY,
       name VARCHAR(255),
       address VARCHAR(255),
       latitude DOUBLE,
